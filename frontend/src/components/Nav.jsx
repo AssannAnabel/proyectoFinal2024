d9bcda3977114efd121fa99ac016bd4fb2391ee9
fabricio <fabricio.cbe@gmail.com>
2024-03-14 16:32:47 -0300

    creacion del nav

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Nav (){
+
+    return(
+
+        <ul>
+            <li>Nosotros</li>
+            <li>Productos</li>
+            <li>Ofertas</li>
+            <li>Contactos</li>
+        </ul>
+    )
+}
+
+export default Nav
