00ff274af57650fae26f7faf39eee8d04133bea9
Anabel <assannanabel@gmail.com>
2024-03-25 15:56:49 -0300

    se instala react-icons, se crea Footer.jsx y Footer.css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { CiInstagram } from "react-icons/ci";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { LiaWhatsappSquare } from "react-icons/lia";
+import { HiOutlineMail } from "react-icons/hi";
+import '../styles/Footer.css'; // Importar el archivo CSS donde escribiremos los estilos
 
-function Footer(){
-    return (<><h2>este es el footer</h2></>)
-
+function Footer() {
+  return (
+    <footer>
+      <div className='social-icons'>
+        <CiInstagram />
+        <AiOutlineFacebook />
+        <LiaWhatsappSquare />
+      </div>
+      <div className='contact-info'>
+        <HiOutlineMail />
+        <p className='p-footer'>agrotech@gmail.com | Benito Juarez</p>
+      </div>
+    </footer>
+  );
 }
+
 export default Footer;
