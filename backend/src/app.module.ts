908b66bb8628b91bcb4e24def705c3e6fbe5fcd6
Fabricio Cordoba <110639727+FabricioCordoba@users.noreply.github.com>
2024-03-26 10:32:40 -0300

    Revert "Logo"

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
-import { ProductModule } from './product/product.module';
-import { InvoiceModule } from './invoice/invoice.module';
-import { InvoicesDetailsModule } from './invoices_details/invoices_details.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'agrotech',
-    entities: [
-      join(__dirname, '/**/*.entity{.js,.ts}')
-    ],
-    synchronize: true
-  }), UserModule, ProductModule, InvoiceModule, InvoicesDetailsModule],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ 
-    whitelist: true,//permite realizar una limpieza de todas las propiedades que no están definidas en el DTO, 
-    //para que el objeto enviado al controlador como body solo tenga las propiedades que se han definido en el DTO.
-    forbidNonWhitelisted: true,
-  }));
   await app.listen(3000);
 }
 bootstrap();
