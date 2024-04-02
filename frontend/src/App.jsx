1824a3d89ffefa87feb1dcd410daf0789c0d00d6
fabricio <fabricio.cbe@gmail.com>
2024-04-02 10:07:21 -0300

    se crea componente CardDescriptionProduct

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About"
 import Contact from "./pages/Contact"
 import  Login  from "./pages/Login"
 import  Register  from "./pages/Register"
+import CardDescriptionProduct from "./components/CardDescriuptionProduct"
 
 
 
@@ -13,6 +14,7 @@ function App() {
   
   return (
     <>
+        <CardDescriptionProduct/>
 
        <Routes>
         <Route path="/" element={ <Home/> } />         
