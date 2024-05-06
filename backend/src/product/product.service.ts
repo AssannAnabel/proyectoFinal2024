4ac850da1f2f854b508972630a2fed170135e309
Emiliano Salazar <salazaremiliano84@gmail.com>
2024-05-06 14:57:41 -0300

    funcion para subir csv casi lista, solo falta retornar la respuesta al cliente

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-import { FindOneOptions, Repository } from 'typeorm';
+import { FindOneOptions, Repository, getManager } from 'typeorm';
 import { Category } from 'src/helpers/enums-type.enum';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -41,6 +41,10 @@ export class ProductService {
     if (!productFound) throw new HttpException({
       status: HttpStatus.NOT_FOUND, error: `No existe un producto con el id ${id}`
     }, HttpStatus.NOT_FOUND)
+    /* const queryProductFound = await this.productRepository.findOne({ where: { product: updateProductDto.product } })
+    if (queryProductFound) throw new HttpException({
+      status: HttpStatus.CONFLICT, error: `el producto ${updateProductDto.product} ya esta cargado en el sistema`
+    }, HttpStatus.CONFLICT) */
     const updateUser = Object.assign(productFound, updateProductDto)
     return this.productRepository.save(updateUser)
   }
@@ -68,12 +72,19 @@ export class ProductService {
   async uploadProductsFromCsv(file: Express.Multer.File) {
     const filePath = path.normalize(file.path);
     let products = [];
+    let errorCount = 0;
 
     fs.createReadStream(filePath)
       .pipe(csv.parse({ headers: true }))
       .on('error', error => console.error(error))
       .on('data', async (row: CreateProductDto) => {
         try {
+          if (!this.isValidProduct(row)) {
+            console.error('Registro no válido:', row);
+            errorCount++;
+            return; // Saltar al siguiente registro
+          }
+
           products.push(
             this.productRepository.create({
               product: row.product,
@@ -83,18 +94,35 @@ export class ProductService {
               amount: row.amount,
               images: row.images
             }));
-          return `Carga exitosa`
         } catch (error) {
-          console.error('Error al guardar en la base de datos:', error);
+          errorCount++;
         }
       })
       .on('end', (rowCount: number) => {
-        console.log(`Se han guardado ${rowCount} filas en la base de datos`);
-        return{ 
-          msg: `Se han guardado ${rowCount} filas en la base de datos`,
-          file: this.productRepository.save(products)
+        console.log(`Se han guardado ${rowCount - errorCount} filas válidas en la base de datos`);
+        if (products.length > 0) {
+          fs.unlinkSync(filePath);
+          this.productRepository.save(products);
+          return { message: `Se han guardado ${rowCount - errorCount} filas válidas en la base de datos de forma exitosa.-`}
+        } else {
+          console.log('No hay productos válidos para guardar');
+          return `Ocurrio un error en uno de los registros. Revise los campos e intente nuevamente.-`
         }
       });
-    
+  }
+
+  isValidProduct(row: CreateProductDto): boolean {
+    if (!row.amount
+      || (row.category in Category)
+      || !row.description
+      || !row.images
+      || !row.price
+      || row.product.length > 45
+    ) {
+      throw new HttpException({
+        status: HttpStatus.BAD_REQUEST, error: `el registro contiene errores. Corrija el error por favor`
+      }, HttpStatus.BAD_REQUEST)
+    }
+    return true;
   }
 }
