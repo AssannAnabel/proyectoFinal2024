60e61a89cbc400bcd7355729288f96d04d253118
fabricio <fabricio.cbe@gmail.com>
2024-05-30 12:38:22 -0300

    mostrar y ocultar compras

--- frontend/src/pages/Perfil.jsx
+++ frontend/src/pages/Perfil.jsx
@@ -5,41 +5,53 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext.jsx';
 import Footer from '../components/Footer.jsx';
 import Nav from '../components/Nav.jsx';
+import '../styles/Perfil.css';
 
 function Perfil() {
   const { user, handleLogout } = useContext(UserContext);
   const navigate = useNavigate();
   const { id } = useParams();
 
+  // Estado inicializado con valores vacíos por defecto
   const [userUpdate, setUserUpdate] = useState({
-    phone: user.telefono,
-    email: user.email,
-    password: user.password,
+    phone: user ? user.telefono : '',
+    email: user ? user.email : '',
+    password: user ? user.password : '',
   });
 
   const [purchaseHistory, setPurchaseHistory] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [tableVisible, setTableVisible] = useState(false); // Estado para controlar la visibilidad de la tabla
+
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
-    async function fetchPurchaseHistory() {
-      try {
-        const purchases = await getUserPurchases(user.id);
-        setPurchaseHistory(purchases);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    }
+    if (user) {
+      setUserUpdate({
+        phone: user.telefono,
+        email: user.email,
+        password: user.password,
+      });
 
-    fetchPurchaseHistory();
-  }, [user.id]);
+      async function fetchPurchaseHistory() {
+        try {
+          const purchases = await getUserPurchases(user.id);
+          setPurchaseHistory(purchases);
+          setLoading(false);
+        } catch (error) {
+          setError(error.message);
+          setLoading(false);
+        }
+      }
+
+      fetchPurchaseHistory();
+    }
+  }, [user]);
 
   function handleChange(e) {
     e.preventDefault();
     setUserUpdate(prev => ({ ...prev, [e.target.name]: e.target.value }));
-    return userUpdate;
   }
 
   const handleSubmit = (e) => {
@@ -54,57 +66,100 @@ function Perfil() {
     navigate('/register');
   };
 
+  if (!user) {
+    return <p>Cargando usuario...</p>;
+  }
+
   return (
     <>
-      <Nav />
-      <div>
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      
-      </div>
+      <div className='container-perfil'>
+        <Nav />
+        <div className='container-'>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
 
-      <div className="container">
-        <h2>Editar perfil</h2>
-        <form className="form" onSubmit={handleSubmit}>
-          <label htmlFor='email'>Email</label>
-          <input type='text' name='email' id='email' value={user.email} onChange={handleChange} />
-          <label htmlFor='phone'>Telefono</label>
-          <input type='phone' name='phone' id='phone' value={userUpdate.phone} onChange={handleChange} />
-          <label htmlFor='password'>Contraseña</label>
-          <input type='password' name='password' id='password' value={userUpdate.password} onChange={handleChange} />
-          <button className='btn-guardar' type='submit'>Guardar</button>
-          <button className='btn-delete' onClick={handleDeleteProfile}>Eliminar perfil</button>
-        </form>
-      </div>
+        <div className="container-form">
+         
+            <button onClick={() => setShowForm(!showForm)}>
+              {showForm ? 'Ocultar perfil' :'Mostrar perfil'}
+            </button>
+          
+          {showForm && ( // Solo muestra el formulario si showForm es true
+            <form className="form" onSubmit={handleSubmit}>
+              <label htmlFor='email'>Email</label>
+              <input type='text' name='email' id='email' value={userUpdate.email || ''} onChange={handleChange} />
+              <label htmlFor='phone'>Telefono</label>
+              <input type='phone' name='phone' id='phone' value={userUpdate.phone || ''} onChange={handleChange} />
+              <label htmlFor='password'>Contraseña</label>
+              <input type='password' name='password' id='password' value={userUpdate.password || ''} onChange={handleChange} />
+              <button className='btn-guardar' type='submit'>Guardar</button>
+              <button className='btn-delete' type='button' onClick={handleDeleteProfile}>Eliminar perfil</button>
+            </form>
+          )}
+        </div>
 
-      <div className="container">
-        <h2>Historial de compras</h2>
-        {loading && <p>Cargando...</p>}
-        {error && <p>Error: {error}</p>}
-        <ul>
-          {purchaseHistory.map(purchase => (
-            <li key={purchase.idInvoice}>
-             <p>Fecha: {new Date(purchase.invoiceDate).toLocaleDateString('es-ES', { day: '2-digit', month: '2-digit', year: 'numeric' })}</p>
-              <p>Productos:</p>
-              <ul>
-                {purchase.invoiceDetails.map(detail => (
-                  <li key={detail.id}>
-                    <p>Nombre: {detail.product.product}</p>
-                    <p>Cantidad: {detail.amount_sold}</p>
-                    <p>Valor unitario: {detail.product.price}</p>
-                  </li>
-                ))}
-              </ul>
-              <p>Total de la compra: {purchase.total_with_iva}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div className="container-purchase-history">
 
-      <Footer />
+
+          <button onClick={() => setTableVisible(!tableVisible)}> {/* Botón para mostrar/ocultar la tabla */}
+            {tableVisible ? 'Ocultar historial de compras' : 'Mostrar historial de compras'}
+          </button>
+
+          {loading && <p>Cargando...</p>}
+          {error && <p>Error: {error}</p>}
+          {tableVisible && (
+            <div className='puserchase'>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Fecha de Factura</th>
+                    <th>Total Sin IVA</th>
+                    <th>Total Con IVA</th>
+                    <th>Producto</th>
+                    <th>Descripción</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {purchaseHistory.map((invoice) => (
+                    <tr key={invoice.idInvoice}>
+                      <td>{new Date(invoice.invoiceDate).toLocaleDateString()}</td>
+                      <td>${invoice.total_without_iva}</td>
+                      <td>${invoice.total_with_iva}</td>
+                      <td>
+                        {invoice.invoiceDetails.map((detail) => (
+                          <p key={detail.id}>{detail.product.product}</p>
+                        ))}
+                      </td>
+                      <td>
+                        {invoice.invoiceDetails.map((detail) => (
+                          <p key={detail.id}>{detail.product.description}</p>
+                        ))}
+                      </td>
+                      <td>
+                        {invoice.invoiceDetails.map((detail) => (
+                          <p key={detail.id}>${detail.product.price}</p>
+                        ))}
+                      </td>
+                      <td>
+                        {invoice.invoiceDetails.map((detail) => (
+                          <p key={detail.id}>{detail.amount_sold}</p>
+                        ))}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+
+        <Footer />
+      </div>
     </>
   );
 }
 
 export default Perfil;
-
