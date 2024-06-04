64f1803b5981d9f022318a28a6982b5ec8de2a93
Anabel <assannanabel@gmail.com>
2024-06-04 20:01:41 -0300

    estilado en about y footer

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,22 +3,25 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import '../styles/About.css'; 
 
-
 function About() {
-    return (
-        <>
-            <div>
-                <Nav />
-            </div>
-            <div className="about-container">
-                <p>Agrotech, tienda en línea especializado en la venta de ropa de trabajo, herramientas de ferretería y artículos para el campo ofrece una solución integral para trabajadores de diferentes industrias. Con una amplia gama de productos cuidadosamente seleccionados y organizados en un catálogo intuitivo, los usuarios pueden explorar fácilmente y encontrar todo lo que necesitan para sus labores diarias.
-                    La plataforma brinda una experiencia de usuario fluida y segura, desde el proceso de registro hasta la finalización de la compra. Los clientes pueden disfrutar de funciones como la búsqueda avanzada, la gestión del carrito de compras y la opción de guardar productos para futuras compras. Además, el sistema de pago integrado garantiza transacciones seguras y sin problemas.
-                    Con un enfoque en la satisfacción del cliente, Agrotech ofrece un servicio de atención al cliente dedicado para resolver consultas, manejar problemas y facilitar devoluciones si es necesario. Además, la plataforma se mantiene actualizada con las últimas medidas de seguridad para proteger la privacidad y la seguridad de los usuarios en todo momento.
-                    En resumen, proporciona una solución conveniente y confiable para trabajadores de diversas industrias, permitiéndoles adquirir fácilmente todo lo que necesitan para su trabajo diario con solo unos pocos clics.
-                </p>
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Nav />
+      <div className="main-content">
+        <div className="about-container">
+          <p>
+            Agrotech es un proyecto de plataforma de comercio electrónico diseñado para el sector agropecuario. Se ha desarrollado utilizando tecnologías modernas como React para el frontend, NestJS para el backend y MySQL como base de datos.
+            El objetivo principal de Agrotech es facilitar a los usuarios la compra de productos agropecuarios de manera eficiente y conveniente, así como brindar a los administradores herramientas para gestionar el inventario y mantener una comunicación efectiva con los clientes.
+            Realizamos este proyecto basándonos en un comercio con un nombre de fantasía, nuestra idea es poder reutilizar este proyecto y adaptarlo a cualquier otro rubro.
+          </p>
+          
+          <div className="line-separator"></div>
+          <p>INTEGRANTES</p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
-export default About
+
+export default About;
