99cfaa0f11c9597f68be4f730630c73238b8a2ac
Anabel <assannanabel@gmail.com>
2024-06-28 17:15:22 -0300

    se elimina back

--- backend/src/auth/interface/auth.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Rol } from "src/helpers/enums-type.enum";
-
-export interface IAccess_token {
-    access_token: string,
-    email: string,
-    name: string,
-    rol: Rol,
-    id: number
-}
